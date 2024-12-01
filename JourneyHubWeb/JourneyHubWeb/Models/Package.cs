f80923cd6a3b869c0b911f21502241bb1118540e
Angeltplaybarber <tejedaangel541@gmail.com>
2024-12-01 19:08:13 -0400

    Version Web demo JourneyHub
    hecho

---
+++ JourneyHubWeb/JourneyHubWeb/Models/Package.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JourneyHubWeb.Models;
+
+public partial class Package
+{
+    public int PackageId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int? TypeId { get; set; }
+
+    public virtual ICollection<PackagePricing> PackagePricings { get; set; } = new List<PackagePricing>();
+
+    public virtual ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+    public virtual PackageType? Type { get; set; }
+}
---
+++ JourneyHubWeb/JourneyHubWeb/Models/PackagePricing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JourneyHubWeb.Models;
+
+public partial class PackagePricing
+{
+    public int PriceId { get; set; }
+
+    public int? PackageId { get; set; }
+
+    public decimal PricePerPerson { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    public virtual Package? Package { get; set; }
+}
